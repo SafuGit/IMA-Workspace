b4b5d2babdf72e3fbbb2233e824fe71baba5a174
Safwan Sadid <safwansadid.ah@gmail.com>
2026-09-06 13:13:54 +0600

    refactor: update init

--- main-tool/email_generation/__init__.py
+++ main-tool/email_generation/__init__.py
@@ -1,4 +1,4 @@
 from .transcript import get_video_data
-from .generate_email import generate_hook_and_email, run_pipeline
+from .generate_email import generate_hook_and_email, run_pipeline, build_agy_prompt
 
-__all__ = ["get_video_data", "generate_hook_and_email", "run_pipeline"]
+__all__ = ["get_video_data", "generate_hook_and_email", "run_pipeline", "build_agy_prompt"]
